c418fd394bea4c3e9d5b728cf8f4b7d854ee06e1
lorette-m <lory_works@outlook.com>
2025-04-12 20:22:53 +0300

    2 Commit. Добавлена обработка ошибки авторизации, разделение на функции, цикл в main

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-
-# import pandas as pd ?? ну пока не нужен
+from selenium.common.exceptions import UnexpectedAlertPresentException
 
 import time
 from bs4 import BeautifulSoup
@@ -12,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from fake_useragent import UserAgent
 
+### Настройка браузера / user-agent а / установка целевой страницы ### Переменные глобальные!
 # Создаём объект для генерации случайных User-Agent’ов
 ua = UserAgent()
 
@@ -21,30 +21,72 @@ options.add_argument(f"--user-agent={ua.random}")  # Устанавливаем 
 
 browser = webdriver.Firefox(options=options)
 
-try:
-    browser.get('https://www.elibrary.ru/authors.asp')
-    print(f"Using User-Agent: {browser.execute_script('return navigator.userAgent;')}")  # Проверка User-Agent
+browser.get('https://www.elibrary.ru/authors.asp')
+print(f"Using User-Agent: {browser.execute_script('return navigator.userAgent;')}")  # Проверка User-Agent
 
-    # surname это поле Фамилия, надо ещё добавить SPIN-код
-    surname_field = browser.find_element(By.ID, 'surname')
+class AuthorizationException(Exception):
+    pass
 
-    surname_to_search = input('Введите фамилию : ')
-    surname_field.send_keys(surname_to_search)
+### Функция поиска. Вызов может повторяться, в т.ч., если найдено >1 автора
+def search_cycle():
+    try:
+        # ДОБАВИТЬ ПРОВЕРКУ АВТОРИЗАЦИИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # id = win_login, уже не помню где я его там нашел, можешь чекнуть через поиск в инспекции HTML-кода страницы
+        # этот id общий для формы вроде, на поля там одинаковый id если я не ошибаюсь, нужно придумать как их заполнить
+        # и тыкнуть кнопочку и ура пользователь авторизирован
+        # Матвей сделай пж проверку на этот id
+        # я сделал в main (изначально бесконечный) цикл, чтобы при ошибке, можно было заново начать поиск, чтобы не запускать скрипт заново
+        # не знаю оставим этот подход или нет, но пока
+        # нужно сделать проверку есть ли элемент с этим id на сайте :
+        ## если есть - пользователь не авторизован, напиши заполнение этой формы (чтобы данные вводить в консоль) и авторизацию
+        ## если нет - пользователь авторизован, все хорошо, продолжаем выполнение цикла
+        # upd. лучше напиши авторизацию отдельной функцией
 
-    code_field = browser.find_element(By.ID, 'codevalue')
+        surname_field = browser.find_element(By.ID, 'surname')
 
-    code_to_search = input('Введите SPIN-код : ')
-    code_field.send_keys(code_to_search + Keys.RETURN)
+        surname_to_search = input('Введите фамилию : ')
+        surname_field.send_keys(surname_to_search)
 
-    # butred это кнопка Поиск
-    share = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.CLASS_NAME, 'butred'))
-    )
-    share.click()
+        code_field = browser.find_element(By.ID, 'codevalue')
 
-    # Даём время загрузиться (можно заменить на WebDriverWait)
-    time.sleep(2)
+        code_to_search = input('Введите SPIN-код : ')
+        code_field.send_keys(code_to_search + Keys.RETURN)
 
-except Exception as e:
-    print(f"Ошибка: {e}")
+        # butred это кнопка Поиск
+        share = WebDriverWait(browser, 10).until(
+           EC.element_to_be_clickable((By.CLASS_NAME, 'butred'))
+        )
+        share.click()
 
+        WebDriverWait(browser, 5).until(EC.alert_is_present())
+
+        alert = browser.switch_to.alert
+        if alert:
+            print("Alert text:", alert.text)
+            alert.dismiss()
+            print("Alert dismissed... Throwing an exception")
+            raise AuthorizationException("Ошибка авторизации. Для парсинга необходимо авторизироваться в eLibrary")
+
+        authors_table = browser.find_element(By.ID, 'restab')
+        rows_in_table = authors_table.find_element(By.TAG_NAME, 'tr')
+        if len(rows_in_table) > 4:
+            print('Найдено более одного автора по данному запросу. Уточните полное ФИО или код автора')
+
+        # Даём время загрузиться (можно заменить на WebDriverWait)
+        time.sleep(2)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+def main():
+    loop_limit = 3
+    loop_iteration = 0
+    while loop_iteration < loop_limit:
+        try:
+            if search_cycle(): # При успешном выполнении выйдет из цикла
+                break
+        except Exception as e:
+            print(f"Произошла ошибка: {e}")
+        loop_iteration += 1
+
+if __name__ == "__main__":
+    main()
