18acd135090b54305dd9cbb65fbfe8da8e0d4112
lorette-m <lory_works@outlook.com>
2025-04-19 18:49:29 +0300

    Commit 6
    1. Добавлен чекбокс "Запомнить меня" в authorize().
    2. Большая доработка search_cycle(): сохранение данных со страницы статистики, переход на страницу статей, выбор категорий, поиск.
    3. Улучшены кастомные исключения, логика обработки исключений перемещена в main().
    4. Добавлена документация на функции и исключения.

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import UnexpectedAlertPresentException, NoSuchElementException, TimeoutException
-
-import time
-import traceback
-from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -23,98 +19,286 @@ options.add_argument(f"--user-agent={ua.random}")  # Устанавливаем 
 
 browser = webdriver.Firefox(options=options)
 
+# Размер и позиция для правой половины экрана (у меня 1920x1200, масштаб 125%)
+browser.set_window_size(768, 920)  # 960/1.25, 1160/1.25
+browser.set_window_position(768, 0)  # (960-12)/1.25 Имитация перемещения окна
+
 browser.get('https://www.elibrary.ru/authors.asp')
 print(f"Using User-Agent: {browser.execute_script('return navigator.userAgent;')}")  # Проверка User-Agent
 
 class AuthorizationException(Exception):
-    pass
+    """Исключение, возникающее при проблемах с авторизацией."""
+    def __init__(self, message="Не удалось авторизоваться. Проверьте введенные данные."):
+        self.message = message
+        super().__init__(self.message)
+class AuthorTableMoreOneRow(Exception):
+    """Исключение, возникающее, если найдено слишком много авторов."""
+    def __init__(self, message="Найдено более одного автора по данному запросу. Уточните полное ФИО или код автора"):
+        self.message = message
+        super().__init__(self.message)
 
 def authorize():
+    """
+    Функция авторизации, вызывается если пользователь не авторизован (на странице обнаружен элемент с id 'win_login'). \n
+    Запрашивает ввод данных, заполняет форму авторизациии, кликает на кнопку входа.
+    """
     try:
-    # Функция авторизации кароче если на странице обнаружен элемент с id 'win_login', значит
-    # gользователь не авторизован беда вообще запрашивает ввод данных, заполняет форму авторизации
-    # и потом кликает на кнопку входа
-        
-        # ЕСЛИ КАПЧА ТО БЕДА БЕДОВАЯ
         login_container = browser.find_element(By.ID, 'win_login')
-        print("Пользователь не авторизован. Переходим к авторизации.")
+        print("Пользователь не авторизован. Переходим к авторизации...")
         
         login_field = login_container.find_element(By.ID, 'login')
         password_field = login_container.find_element(By.ID, 'password')
-        
-        user_login = input('Введите логин или почту: ')
-        user_password = input('Введите пароль: ')
-        
+
+        # Мои учетные данные для автозаполнения
+        user_login = "Loretta Mosshart"
+        user_password = "lory_study"
+
+        #user_login = input('Введите логин или почту: ')
+        #user_password = input('Введите пароль: ')
+
         login_field.clear()
         login_field.send_keys(user_login)
         password_field.clear()
         password_field.send_keys(user_password)
 
+        # Чекбокс "Запомнить меня"
+        checkbox = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'knowme'))
+        )
+        if not checkbox.is_selected():
+            checkbox.click()
+            print("Чекбокс \"Запомнить меня\" отмечен.")
+        else:
+            print("Чекбокс \"Запомнить меня\" уже установлен.")
+
         login_button = login_container.find_element(By.CLASS_NAME, 'butred')
         login_button.click()
-        
-        time.sleep(2)
-        browser.get('https://www.elibrary.ru/authors.asp')
+
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, 'surname')))
         print("Авторизация выполнена.")
-    except Exception as e:
-        print("Ошибка при авторизации:")
-        traceback.print_exc()
-        raise AuthorizationException("Не удалось авторизоваться. Проверьте введенные данные.")
+    except (TimeoutException, NoSuchElementException) as e:
+        raise AuthorizationException("Не удалось авторизоваться. Проверьте элементы формы.") from e
 
 def search_cycle():
+    """
+    Основная функция работы с сайтом, выполняющая поиск, сохранение статистики и нужной веб-страницы.
+     1. Проверка на авторизацию.
+     2. Поиск по Фамилии и SPIN-коду.
+     3. Проверка таблицы результатов поиска (должен быть один автор).
+     4. Сбор количества статей со страницы статистики автора.
+     5. Переход на страницу статей, выбор категорий, поиск, сохранение веб-страницы.
+    NB: В графу "Фамилия" можно указать полное ФИО, допустимо пропустить SPIN-код, но тогда может быть несколько авторов с одинаковым ФИО.
+    """
     try:
         try:
             browser.find_element(By.ID, 'win_login')
             authorize()
         except NoSuchElementException:
             print("Пользователь уже авторизован.")
-        
-        surname_field = browser.find_element(By.ID, 'surname')
-        surname_field.clear()
-        surname_to_search = input('Введите фамилию : ')
-        surname_field.send_keys(surname_to_search)
 
-        code_field = browser.find_element(By.ID, 'codevalue')
+        surname_field = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'surname'))
+        )
+        surname_field.clear()
+        code_field = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'codevalue'))
+        )
         code_field.clear()
-        code_to_search = input('Введите SPIN-код : ')
+
+        # Тестовые данные: Лукин 7065-3430
+        surname_to_search = "Лукин"
+        code_to_search = "7065-3430"
+
+        #surname_to_search = input('Введите фамилию : ')
+        #code_to_search = input('Введите SPIN-код : ')
+
+        surname_field.send_keys(surname_to_search)
         code_field.send_keys(code_to_search + Keys.RETURN)
 
-        # butred это кнопка Поиск
         share = WebDriverWait(browser, 10).until(
            EC.element_to_be_clickable((By.CLASS_NAME, 'butred'))
         )
         share.click()
 
-        WebDriverWait(browser, 5).until(EC.alert_is_present())
-
-        alert = browser.switch_to.alert
-        if alert:
+        try:
+            WebDriverWait(browser, 5).until(EC.alert_is_present())
+            alert = browser.switch_to.alert
             print("Alert text:", alert.text)
             alert.dismiss()
-            print("Alert dismissed... Throwing an exception")
             raise AuthorizationException("Ошибка авторизации. Для парсинга необходимо авторизироваться в eLibrary")
+        except TimeoutException:
+            print("Alert не появился, продолжаем выполнение...")
 
-        # authors_table = browser.find_element(By.ID, 'restab')
-        # rows_in_table = authors_table.find_element(By.TAG_NAME, 'tr')
-        # if len(rows_in_table) > 4:
-        #    print('Найдено более одного автора по данному запросу. Уточните полное ФИО или код автора')
+        ### Работа с таблицей
 
-        # Даём время загрузиться (можно заменить на WebDriverWait)
-        time.sleep(2)
+        authors_table = WebDriverWait(browser, 15).until(
+            EC.presence_of_element_located((By.ID, 'restab'))
+        )
+        rows_in_table = authors_table.find_elements(By.TAG_NAME, 'tr')
+        print(f"Найдено строк в таблице: {len(rows_in_table)}")
+        if len(rows_in_table) > 4:
+            raise AuthorTableMoreOneRow()
+
+        ### Собираем количество статей на странице со статистикой
+
+        statistical_page = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((
+            By.XPATH,
+            '//a[contains(@href, "author_profile.asp?id=") and contains(@title, "Анализ публикационной активности")]'
+            ))
+        )
+        statistical_page.click()
+        print("Страница статистики загружена.")
+
+        # Количество всех публикаций
+        publications_total_elib = WebDriverWait(browser, 15).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                '//a[contains(@href, "author_items.asp") and contains(@title, "Полный список публикаций")]'
+            ))
+        ).text
+
+        # Публикаций в РИНЦ
+        publications_rinc = WebDriverWait(browser, 15).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                '//a[contains(@href, "author_items.asp") and contains(@title, "Список публикаций автора в РИНЦ")]'
+            ))
+        ).text
+
+        # Публикаций в ядре РИНЦ
+        publications_rinc_core = WebDriverWait(browser, 15).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                '//a[contains(@href, "author_items.asp") and contains(@title, "Список публикаций данного автора, входящих в ядро РИНЦ")]'
+            ))
+        ).text
+
+        browser.back()
+
+        ### Переходим на страницу с публикациями в РИНЦ
+
+        rinc_link = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((
+                By.XPATH,
+                '//a[contains(@href, "author_items.asp") and contains(@title, "Список публикаций данного автора в РИНЦ")]'
+            ))
+        )
+
+        original_window = browser.current_window_handle
+
+        rinc_link.click()
+
+        WebDriverWait(browser, 15).until(EC.number_of_windows_to_be(2))
+
+        for window_handle in browser.window_handles:
+            if window_handle != original_window:
+                browser.switch_to.window(window_handle)
+
+        print("Перешли на страницу списка публикаций в РИНЦ.")
+
+        types_header = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((By.ID, 'hdr_types'))
+        )
+        types_header.click()
+
+        checkbox_articles = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((By.ID, 'types_4'))
+        )
+        checkbox_articles.click()
+
+        cats_header = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((By.ID, 'hdr_cats'))
+        )
+        cats_header.click()
+
+        categories = [
+            {"id": "cats_risc", "name": "Публикации, включенные в РИНЦ"},
+            {"id": "cats_corerisc", "name": "Публикации, включенные в ядро РИНЦ"},
+            {"id": "cats_whitelist1", "name": "Статьи в журналах, включенных в Белый список (уровень 1)"},
+            {"id": "cats_whitelist2", "name": "Статьи в журналах, включенных в Белый список (уровень 2)"},
+            {"id": "cats_whitelist3", "name": "Статьи в журналах, включенных в Белый список (уровень 3)"},
+            {"id": "cats_whitelist4", "name": "Статьи в журналах, включенных в Белый список (уровень 4)"},
+            {"id": "cats_rsci", "name": "Статьи в журналах, входящих в RSCI"},
+            {"id": "cats_scopus1", "name": "Статьи в журналах, входящих в Scopus (квартиль 1)"},
+            {"id": "cats_scopus2", "name": "Статьи в журналах, входящих в Scopus (квартиль 2)"},
+            {"id": "cats_scopus3", "name": "Статьи в журналах, входящих в Scopus (квартиль 3)"},
+            {"id": "cats_scopus4", "name": "Статьи в журналах, входящих в Scopus (квартиль 4)"}
+        ]
+
+        # Сайт может сохранять состояние чекбоксов, нужно пройтись и проверить, что галочек нет
+        for category in categories:
+            checkbox = WebDriverWait(browser, 5).until(
+                EC.element_to_be_clickable((By.ID, category["id"]))
+            )
+            if checkbox.is_selected():
+                checkbox.click()
+
+        print("Выберите категории публикаций (введите номера через пробел, например: 1 3 5):")
+        for i, category in enumerate(categories, 1):
+            print(f"{i}. {category['name']}")
+
+        # Получаем выбор пользователя
+        selected_indices = input("Ваш выбор: ").split(" ")
+        selected_indices = [int(i.strip()) - 1 for i in selected_indices if i.strip().isdigit()]
+
+        # Проверяем валидность ввода
+        if not selected_indices or max(selected_indices) >= len(categories) or min(selected_indices) < 0:
+            print("Неверный выбор категорий.")
+            return
+
+        # Кликаем на выбранные чекбоксы
+        for index in selected_indices:
+            checkbox_id = categories[index]["id"]
+            checkbox = WebDriverWait(browser, 15).until(
+                EC.element_to_be_clickable((By.ID, checkbox_id))
+            )
+            if not checkbox.is_selected():  # Проверяем, не стоит ли галочка
+                checkbox.click()
+
+        search_button = WebDriverWait(browser, 15).until(
+            EC.element_to_be_clickable((
+                By.XPATH,
+                '//div[contains(@class, "butred") and contains(@onclick, "pub_search()")]'
+            ))
+        )
+        search_button.click()
+
+        return publications_total_elib, publications_rinc, publications_rinc_core
+    except AuthorizationException:
+        raise
+    except AuthorTableMoreOneRow:
+        raise
     except Exception as e:
-        print(f"Ошибка: {e}")
+        print(f"Неожиданная ошибка: {e}")
+        return None
 
 def main():
-    loop_limit = 3
+    loop_limit = 1
     loop_iteration = 0
     while loop_iteration < loop_limit:
         try:
-            if search_cycle(): # При успешном выполнении выйдет из цикла
+            result = search_cycle()
+            if result: # При успешном выполнении выйдет из цикла
+                # Аня, я вернул тебе данные из search_cycle()
+                publications_total_elib, publications_rinc, publications_rinc_core = result
+                print("Функция search_cycle() завершена успешно.")
+                print(f"Число публикаций на elibrary.ru: {publications_total_elib}\n"
+                      f"Число публикаций в РИНЦ: {publications_rinc}\n"
+                      f"Число публикаций, входящих в ядро РИНЦ: {publications_rinc_core}")
                 break
+        except AuthorizationException as e:
+            print(f"Ошибка авторизации: {e}")
+        except AuthorTableMoreOneRow as e:
+            print(f"Ошибка поиска авторов: {e}")
         except Exception as e:
-            print(f"Произошла ошибка: {e}")
+            print(f"Неожиданная ошибка: {e}")
         loop_iteration += 1
+    else:
+        print(f"Достигнут лимит попыток ({loop_limit}).")
+    #browser.quit()
+    print("Программа выполнена.")
+
 
 if __name__ == "__main__":
     main()
